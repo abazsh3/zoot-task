11691395fff9b077e4ae97bb32095706755fa0cc
abazsh3 <abazsh3@gmail.com.com>
2023-06-08 11:42:34 +0330

    config redux store and RTK query todos service

---
+++ src/services/todoService.ts
@@ -0,0 +1,17 @@
+// Need to use the React-specific entry point to import createApi
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import type { Todo } from "./types";
+
+export const todoApi = createApi({
+  reducerPath: "todoApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://127.0.0.1:3000/" }),
+  tagTypes: ["get-todos"],
+  endpoints: (builder) => ({
+    getTodos: builder.query<Todo, void>({
+      query: () => "/todos",
+      providesTags: ["get-todos"],
+    }),
+  }),
+});
+
+export const { useGetTodosQuery } = todoApi;
